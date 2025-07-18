c508410b434e1c59f7e04d6a5d9dce71a92ef4bd
apple_star18 <danielbell0x03@gmail.com>
2025-07-18 01:52:04 +0100

    update_config_build_trait

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -9,23 +9,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("not enough argument");
-        }
+    pub fn build(
+            mut args: impl Iterator<Item = String>
+        ) -> Result<Config, &'static str> {
+            args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+            let query = match args.next() {
+                Some(args) => args,
+                None => return Err("Didn't get a query string"),
+            };
 
-        //true if the IGNORE_CASE environment variable is **set**
+            let file_path = match args.next() {
+                Some(args) => args,
+                None => return Err("Didn't get a file path"),
+            };
 
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
+            let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
+            Ok(Config {
+                query,
+                file_path,
+                ignore_case,
+            })
     }
 }
 
